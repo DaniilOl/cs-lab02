9e0cda32ed5efd6a3e8a040f85216cea2299db0f
Alice (Danya&Ura) <alice@example.com>
2023-06-21 16:30:10 +0300

    code: обновление

--- Task#1.cpp
+++ Task#1.cpp
@@ -10,8 +10,8 @@ int main() {
 	std::cout << "Enter A and B: ";
 	int a, b;
 	std::cin >> a >> b;
-    std::cout << a + b;
-
+    std::cout << "A + B = " << a + b << '\n'
+			  << "A - B = " << a - b << '\n';
 
 
 
